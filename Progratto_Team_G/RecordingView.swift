b02afa5946dd9b6c82421a893a7a33d9eeea6301
stefano <stefanoverrilli00@gmail.com>
2022-04-11 23:08:45 +0200

    Fixed Recording page spaces, and modified Tabbar

--- Progratto_Team_G/RecordingView.swift
+++ Progratto_Team_G/RecordingView.swift
@@ -10,22 +10,20 @@ import SwiftUI
 
 struct RecordingView: View{
     var body:some View{
-             VStack(spacing:0){
-                 HStack{
+             VStack(spacing:100){
+                 VStack{
+                     HStack{
                      Text("Track your hives")
                          .font(.system(size: 32, weight: .bold))
                          .multilineTextAlignment(.leading)
                          .frame(alignment:.leading)
                          Spacer()
-                 }.padding()
-                 
-                 HStack{
+                     }
                   Text("Shake the iPhone to record \nor press the mic")
                       .multilineTextAlignment(.leading)
                       .frame(maxWidth:.infinity,alignment: .leading)
                       .font(.system(size:26))
-                 }.padding(.leading)
-                 Spacer()
+                 }.padding(.leading).padding(.top)
                  HStack{
                      ZStack{
                             Image(systemName:"circle.fill")
@@ -34,11 +32,11 @@ struct RecordingView: View{
                          Image(systemName:"mic.fill")
                              .font(.system(size:110)).foregroundColor(Color.white).shadow(color: .black, radius: 0, x: 0, y: 0)
                      }
-                 }
-                 Spacer()
+                 }.padding(.vertical)
                  HStack{
                      Text("Futura stringa di speech to text aaaaaaa")
                  }
+                 Spacer()
              }.background(BackgroundView())
     }
 }
--- Progratto_Team_G/Tabbar.swift
+++ Progratto_Team_G/Tabbar.swift
@@ -24,15 +24,9 @@ struct Tabbar: View {
             let appearance = UITabBarAppearance()
             appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
             appearance.backgroundColor = UIColor(red: 237/255.0, green: 194/255.0, blue: 93/255.0, alpha: 1.0)
-            
-            // Use this appearance when scrolling behind the TabView:
             UITabBar.appearance().standardAppearance = appearance
-            // Use this appearance when scrolled all the way up:
             if #available(iOS 15.0, *) {
-                UITabBar.appearance().scrollEdgeAppearance = appearance
-            } else {
-                // Fallback on earlier versions
-            }
+                UITabBar.appearance().scrollEdgeAppearance = appearance}
     }
 }
 struct ContentView_Previews: PreviewProvider {
