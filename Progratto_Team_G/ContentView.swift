5f5d03ed1fd26707d765aba0e7306ebc38006b35
stefano <stefanoverrilli00@gmail.com>
2022-04-12 15:08:28 +0200

    Changes made to try parthenokit

--- Progratto_Team_G/ContentView.swift
+++ Progratto_Team_G/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ParthenoKit
 
 struct ContentView: View{
     init() {
--- Progratto_Team_G/DetailedView/AlternativeHiveDetail.swift
+++ Progratto_Team_G/DetailedView/AlternativeHiveDetail.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import ParthenoKit
 
 struct AlternativeHiveDetail: View{
     init(){
