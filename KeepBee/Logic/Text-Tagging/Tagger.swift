25cbb120615df8c97eead08fffbcdd01c91f59ac
stefano <stefanoverrilli00@gmail.com>
2022-04-19 20:50:15 +0200

    refactor

--- KeepBee/Logic/Text-Tagging/Tagger.swift
+++ KeepBee/Logic/Text-Tagging/Tagger.swift
@@ -2,7 +2,7 @@ import Foundation
 
 var hiveToFill = Hive()
 var hivesNames = LoadArrayOfHives()
-let keyWordToFind = ["looms","loom","nourished","been fed","is orphan","not an orhphan","queen","royal","kg","weighs","weight","diagram","swarm","is an orphan","is orphan","not an orphan","isn't an orphan","isn't orphan","to be fed","to be nourished","been said","to be said","to be sad","been sad","royale","quinn","to be fat","been fat"]
+let keyWordToFind = ["looms","loom","nourished","been fed","is orphan","not an orhphan","queen","royal","kg","weighs","weight","diagram","swarm","is an orphan","is orphan","not an orphan","isn't an orphan","isn't orphan","be fed","be nourished","been said","be said","be sad","been sad","royale","quinn","be fat","been fat"]
 
 func StringMatching(stringToCheck: String) -> [String]{
     let targetHive = hivesNames.filter{stringToCheck.lowercased().range(of: "(?<![\\w\\d])\($0.hiveName)(?![\\w\\d])",options: [ .regularExpression,.caseInsensitive,.backwards]) != nil}
@@ -45,7 +45,7 @@ func CaseClassifier(CompleteString : String,ParticularCase: String){
         let regex = "[0-9]{1,2}"
         let result = DetectNumsInString(StringToCheck: String(substring), CompleteString: CompleteString, KeyWord: ParticularCase,regex: regex)
         if result != nil{hiveToFill.loomsInside = result!}
-    case "to be fed","to be nourished","to be said","to be sad","to be fat":
+    case "be fed","be nourished","be said","be sad","be fat":
         let range = CompleteString.range(of: "\(ParticularCase)",options: [.backwards,.caseInsensitive,.regularExpression])?.lowerBound
         let substring = CompleteString[range!...]
         let result = DetectDatesInString(StringToCheck:String(substring))
